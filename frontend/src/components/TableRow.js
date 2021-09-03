247d23111edd0c467a7e6d16b187c4e13cc8b111
sanduni7 <gpssara97@gmail.com>
2021-09-03 22:25:15 +0530

    tableRow updated

--- frontend/src/components/TableRow.js
+++ frontend/src/components/TableRow.js
@@ -1,33 +1,67 @@
-import React,{Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
-class TableRow extends Component{ // create table two component to get the patient details into a table
-    render(){
-        return (
-            <tr>
-                <td>
-                    {this.props.obj.patient_name}
-                </td>
-                <td>
-                    {this.props.obj.contactNo}
-                </td>
-                <td>
-                    {this.props.obj.Email}
-                </td>
-                <td>
-                    {this.props.obj.title}
-                </td>
-                <td>
-                    {this.props.obj.question}
-                </td>
-                <td>
+class TableRow extends Component {
+  // create table two component to get the patient details into a table
 
-                    <Link to={"/edit/" + this.props.obj._id} className="btn btn-warning btn-sm"><i className="fas fa-edit">Edit</i></Link>
-                </td>
-                
-            </tr>
-        );
+  viewPdf(url) {
+    if (url) {
+      window.location = url;
+    } else{
+        alert("There is no PDF to view");
     }
+  }
+
+  //Delete Method
+    Delete = (id) => {
+      console.log(id);
+      axios
+        .post(
+          "http://localhost:3001/patient/delete/" + id
+        )
+        .then((response) => {
+          alert("Successfully Deleted !");
+          window.location = "/ViewPatients";
+        });
+    };
+  render() {
+    return (
+      <tr>
+        <td>{this.props.obj.patient_name}</td>
+        <td>{this.props.obj.contactNo}</td>
+        <td>{this.props.obj.Email}</td>
+        <td>{this.props.obj.title}</td>
+        <td>{this.props.obj.question}</td>
+        <td>
+          <Link
+            to={"/edit/" + this.props.obj._id}
+            className="btn btn-warning btn-sm"
+          >
+            <i className="fas fa-edit">EDIT</i>
+          </Link>
+        </td>
+        <td>
+          <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => this.Delete(this.props.obj.id)}
+                      style={{ color: "black" }}
+                    >
+                      <i className="fas fa-trash"></i> DELETE
+                    </button>
+        </td>
+
+        <td>
+          <button
+            className="btn btn-info btn-sm"
+            onClick={() => this.viewPdf(this.props.obj.url)}
+          >
+            <i className="fas fa-file-pdf"></i> VIEW PDF
+          </button>
+        </td>
+      </tr>
+    );
+  }
 }
 
 export default TableRow;
